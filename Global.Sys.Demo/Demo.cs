6a82a8207e237de3e1c843464cf88761e20ec768
javacommons <javacommons@gmail.com>
2026-03-26 20:20:36 +0900

    ⭕️ [RELEASE] v2026.0326.2020.22 [DIFF] diff --git a/Global.Sys.Demo/Demo.cs b/Global.Sys.Demo/Demo.cs     -                    "https://www.youtube.com/watch?v=sLpodTN4xhI&list=PLTvSv0jkjbk9-emLIV2vM-0p7CeMnTYG2"     +                    "https://www.youtube.com/watch?v=DHbIIBmqHsw&list=PLTvSv0jkjbk8wtAgpVJH1L21EgeMi_ULc"     -                    "https://www.youtube.com/playlist?list=PLTvSv0jkjbk8gtWLMLXLHYrWio5ciOi8c"     +                    "https://www.youtube.com/watch?v=wzcdhDyNmMM&list=PLTvSv0jkjbk8gtWLMLXLHYrWio5ciOi8c"     -                    "https://www.youtube.com/playlist?list=PLTvSv0jkjbk_u4GZBJK74w7aWylX-8FSt"     +                    "https://www.youtube.com/watch?v=4B5IHILMWOM&list=PLTvSv0jkjbk_u4GZBJK74w7aWylX-8FSt"     +                  );     +                LinkTest(     +                    "⭕️⁅🌐⁆@⁅CHANNEL：〘!!GREAT!!〙Blackpink Diaries⁆⭕️❝BLACKPINK➡️Ice Cream (2026 Official Music Video)❞",     +                    "https://www.youtube.com/watch?v=YwhhB8rKb6U&list=PLTvSv0jkjbk9vEyRq7pK_U8fbGrXirdAi"     +                  );     +                LinkTest(     +                    "⭕️⁅🌐⁆@⁅CHANNEL：〘!!GREAT!!〙Alyssa' s Music Loop⁆⭕️❝🎙 More Than Is Good for Me ( Original ) ✨️ EDM - Electronic Dance Music ✨️ # 179❞",     +                    "https://www.youtube.com/watch?v=qrW3yK7AWjE&list=PLTvSv0jkjbk-8ABf2TXzCXWk7zn10Ute7"     - diff --git a/Global.Sys.Demo/assets.zip b/Global.Sys.Demo/assets.zip diff --git a/Global.Sys/Global.Sys.csproj b/Global.Sys/Global.Sys.csproj     -		<Version>2026.0326.1927.55</Version>     +		<Version>2026.0326.2020.22</Version>     -		<PackageReference Include="EasyObject" Version="2026.326.1848.36" />     +		<PackageReference Include="EasyObject" Version="2026.326.1909.31" /> diff --git a/version.txt b/version.txt     -2026.0326.1927.55     +2026.0326.2020.22 [/DIFF]

--- Global.Sys.Demo/Demo.cs
+++ Global.Sys.Demo/Demo.cs
@@ -234,17 +234,24 @@ namespace Global {
                   );
                 LinkTest(
                     "⭕️🈂️❝FG⁅ｼﾞﾝｷﾞｽｶﾝ⁆❞🈂️ファイターズガール「ジンギスカン」踊ってみた 歌詞付き",
-                    "https://www.youtube.com/watch?v=sLpodTN4xhI&list=PLTvSv0jkjbk9-emLIV2vM-0p7CeMnTYG2"
+                    "https://www.youtube.com/watch?v=DHbIIBmqHsw&list=PLTvSv0jkjbk8wtAgpVJH1L21EgeMi_ULc"
                   );
                 LinkTest(
                     "⭕️⁅🌐⁆@ラム:DANCING STAR 2026",
-                    "https://www.youtube.com/playlist?list=PLTvSv0jkjbk8gtWLMLXLHYrWio5ciOi8c"
+                    "https://www.youtube.com/watch?v=wzcdhDyNmMM&list=PLTvSv0jkjbk8gtWLMLXLHYrWio5ciOi8c"
                   );
                 LinkTest(
                     "⭕️⁅🌐⁆@エレクトロニック・ダンス・ミュージック",
-                    "https://www.youtube.com/playlist?list=PLTvSv0jkjbk_u4GZBJK74w7aWylX-8FSt"
+                    "https://www.youtube.com/watch?v=4B5IHILMWOM&list=PLTvSv0jkjbk_u4GZBJK74w7aWylX-8FSt"
                   );
-
+                LinkTest(
+                    "⭕️⁅🌐⁆@⁅CHANNEL：〘!!GREAT!!〙Blackpink Diaries⁆⭕️❝BLACKPINK➡️Ice Cream (2026 Official Music Video)❞",
+                    "https://www.youtube.com/watch?v=YwhhB8rKb6U&list=PLTvSv0jkjbk9vEyRq7pK_U8fbGrXirdAi"
+                  );
+                LinkTest(
+                    "⭕️⁅🌐⁆@⁅CHANNEL：〘!!GREAT!!〙Alyssa' s Music Loop⁆⭕️❝🎙 More Than Is Good for Me ( Original ) ✨️ EDM - Electronic Dance Music ✨️ # 179❞",
+                    "https://www.youtube.com/watch?v=qrW3yK7AWjE&list=PLTvSv0jkjbk-8ABf2TXzCXWk7zn10Ute7"
+                  );
 #if false
                 Sys.Crash();
 #endif
