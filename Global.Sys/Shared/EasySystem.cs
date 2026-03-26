47274f533587ae82c4aa99d79b6e230edd3748fa
javacommons <javacommons@gmail.com>
2026-03-26 19:28:04 +0900

    ⭕️ [RELEASE] v2026.0326.1927.55 [DIFF] diff --git a/Global.Sys/Global.Sys.csproj b/Global.Sys/Global.Sys.csproj     -		<Version>2026.0326.1729.01</Version>     +		<Version>2026.0326.1927.55</Version>     -		<PackageReference Include="EasyObject" Version="2026.326.1710.57" />     +		<PackageReference Include="EasyObject" Version="2026.326.1848.36" /> diff --git a/Global.Sys/Shared/EasySystem.cs b/Global.Sys/Shared/EasySystem.cs     -            // [⭕ファイル名に使えない文字 - Google](https://bit.ly/invalid-filename-chars)     -            // \ / : * ? " < > |     -            baseName = baseName     -                .Replace("\\", "￥")     -                .Replace("/", "／")     -                .Replace(":", "：")     -                .Replace("*", "＊")     -                .Replace("?", "❓")     -                .Replace("\"", "“")     -                .Replace("<", "≪")     -                .Replace(">", "≫")     -                .Replace("|", "￤")     -                ;     -            return baseName;     +            return UniversalTransformer.SafeBaseName(baseName, followRecommendation: false); diff --git a/version.txt b/version.txt     -2026.0326.1729.01     +2026.0326.1927.55 [/DIFF]

--- Global.Sys/Shared/EasySystem.cs
+++ Global.Sys/Shared/EasySystem.cs
@@ -475,20 +475,7 @@ namespace Global {
             Environment.SetEnvironmentVariable(name, value);
         }
         public static string SafeBaseName(string baseName) {
-            // [⭕ファイル名に使えない文字 - Google](https://bit.ly/invalid-filename-chars)
-            // \ / : * ? " < > |
-            baseName = baseName
-                .Replace("\\", "￥")
-                .Replace("/", "／")
-                .Replace(":", "：")
-                .Replace("*", "＊")
-                .Replace("?", "❓")
-                .Replace("\"", "“")
-                .Replace("<", "≪")
-                .Replace(">", "≫")
-                .Replace("|", "￤")
-                ;
-            return baseName;
+            return UniversalTransformer.SafeBaseName(baseName, followRecommendation: false);
         }
         public static string HomeFile(params string[] relatives) {
             string home = GetEnv("HOME", "");
